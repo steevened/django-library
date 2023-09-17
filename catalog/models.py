daf4eb940160ac3f92d7d690fa6ec2d644045d3b
steven <alvaradosteven315@gmail.com>
2023-09-17 15:24:59 -0500

    create genre and book class models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200, help_text="Enter a book genre")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(
+        max_length=1000, help_text="Enter a brief description of a book")
+    isbn: models.CharField('ISBN', max_length=13, unique=True,
+                           help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>'
+                           )
+    genre = models.ManyToManyField(Genre, help_text="Select a genre")
+
+    def __str__(self) -> str:
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])

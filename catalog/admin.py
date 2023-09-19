84c073a3232329e5cef77d676d9ef3f08d8fdb86
steevened <alvaradosteven315@gmail.com>
2023-09-18 21:26:03 -0500

    change model admin views

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Author, Book, BookInstance, Genre, Language
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(BookInstance)
 admin.site.register(Genre)
 admin.site.register(Language)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
